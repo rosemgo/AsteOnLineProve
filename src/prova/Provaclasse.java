ad0a3e7aa249f5aef81b9688c04b1830b5c66d05
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2019-11-13 20:36:45 +0100

    1 modifica

--- src/prova/Provaclasse.java
+++ src/prova/Provaclasse.java
@@ -12,15 +12,15 @@ public class Provaclasse {
 
 	//AGGIUNGO QUESTA LINEA, DOPO IL PUSH FATTO DAL MASTER    modifica linea fatta da marco 19:47. 
 
-	//HO CANCELLATO LE CAZZATE DI MARCO
+	//HO CANCELLATO LE CAZZATE DI MARCO E SONO CONTENTO
+	
+	//non ti responn proprio
+	
+	//CHE FAI ti sto pensando ee prendendo per culo
 	
 	//WE
 	
-	//CHE FAI
-	
-	//WE
-	
-	//ANCORA WE
+	//sti cinisi i merd
 		
 	//ci SEI UN COGLIONS
 	
