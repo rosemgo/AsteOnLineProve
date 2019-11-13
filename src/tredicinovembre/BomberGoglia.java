835b71382e501160cc50270c2ab0322a551ca327
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2019-11-13 19:38:57 +0100

    primo commit conmarco

---
+++ src/tredicinovembre/BomberGoglia.java
@@ -0,0 +1,19 @@
+package tredicinovembre;
+
+public class BomberGoglia {
+
+	public static void main(String[] args) {
+
+		int bomber = 1;
+		int marco = 1;
+		int result=0;
+		
+		result = bomber+marco;
+		
+		System.out.println("risutato: " + result);
+		//MODIFICA TERMINATA ALLE ORE 19:36
+		
+		
+	}
+
+}
