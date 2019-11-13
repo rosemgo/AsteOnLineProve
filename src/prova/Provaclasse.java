dae9cf001e98638c5f1a4fafe1ddc9d020a1d826
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2019-11-13 20:37:36 +0100

    2 modifica

--- src/prova/Provaclasse.java
+++ src/prova/Provaclasse.java
@@ -8,7 +8,7 @@ public class Provaclasse {
 	
 	public modifica; //HO TOLTO IL COMMENTO, NEL MASTER MODIFICO QUESTA LINEA
 	 
-	//AGGIUNGO UNA LINEA
+	//AGGIUNGO UNA LINEA, non dovrebbe dare errore il rebase
 
 	//AGGIUNGO QUESTA LINEA, DOPO IL PUSH FATTO DAL MASTER    modifica linea fatta da marco 19:47. 
 
