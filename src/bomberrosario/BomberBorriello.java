092d320758e4c57e68cb4e7a831aa19b3aae8bc0
Rosario-VAIO\Rosario <rosariogoglia@gmail.com>
2019-11-13 19:47:22 +0100

    primo commit bomberrosario 19:47

---
+++ src/bomberrosario/BomberBorriello.java
@@ -0,0 +1,15 @@
+package bomberrosario;
+
+public class BomberBorriello {
+
+	public static void main(String[] args) {
+
+		int marco = 3;
+		int rosario=3;
+		
+		int somma = rosario + marco;
+		System.out.println("La somma è: " + somma);
+
+	}
+
+}
